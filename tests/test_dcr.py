91184d6e1a4e43660def49bfa1d8bdd42dd0f00d
DavideScassola <davide.scassola1@gmail.com>
2026-07-10 17:55:37 +0200

    bugfixes

--- tests/test_dcr.py
+++ tests/test_dcr.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 
 from torch_dcr import dcr
 
@@ -17,7 +18,7 @@ def test_dcr1():
         {
             "A": [1, 2, 3, 4, 5],
             "B": ["f", "b", "g", "d", "d"],
-            "C": [0.0, 0.0, 2.0, -12.0, -4.0],
+            "C": [0.0, 0.0, 2.0, -11.0, -4.0],
             "D": ["2", "2", "9", "4", "4"],
         }
     )
@@ -27,10 +28,119 @@ def test_dcr1():
     )
 
     expected_dcr = pd.DataFrame(
-        {"dcr_1": [1.063227, 0.393267, 1.0, 0.943477, 2.006269]}
+        {"dcr_1": [0.7761391, 0.53872633, 0.6627745, 0.3845973, 0.4744165]}
     ).astype("float32")
 
-    expected_index = pd.DataFrame({"index_1": [1, 3, 2, 2, 2]})
+    expected_index = pd.DataFrame({"index_1": [2, 3, 2, 0, 2]})
 
     pd.testing.assert_frame_equal(dcr_df, expected_dcr, check_exact=False, atol=1e-5)
     pd.testing.assert_frame_equal(index_df, expected_index)
+
+
+def test_dcr2():
+    source_df = pd.DataFrame(
+        {
+            "A": [0, 0, -1],
+            "B": ["a", "g", "b"],
+        }
+    )
+
+    target_df = pd.DataFrame(
+        {
+            "A": [0, 0, 0, -0.5],
+            "B": ["a", "b", "c", "e"],
+        }
+    )
+
+    dcr_df, index_df = dcr(
+        source_df,
+        target_df,
+        device="cpu",
+        k=1,
+        output_indexes=True,
+        metric="l1",
+        standardize=False,
+    )
+
+    expected_dcr = pd.DataFrame({"dcr_1": [0.0, 2.0, 1.0]}).astype("float32")
+    expected_index = pd.DataFrame({"index_1": [0, 1, 1]})
+
+    pd.testing.assert_frame_equal(dcr_df, expected_dcr, check_exact=False, atol=1e-5)
+    pd.testing.assert_frame_equal(index_df, expected_index)
+
+
+def test_dcr3():
+    source_df = pd.DataFrame(
+        {
+            "A": ["a", "b", "c"],
+            "B": ["1", "2", "3"],
+        }
+    )
+
+    target_df = pd.DataFrame(
+        {
+            "A": ["a", "a", "b"],
+            "B": ["1", "2", "1"],
+        }
+    )
+
+    expected_dcrs = {
+        "cosine": [0.0, 0.5, 1.0],
+        "euclidean": [0.0, 1.4142135, 2.0],
+        "l1": [0.0, 2.0, 4.0],
+    }
+
+    for metric, expected_dcr in expected_dcrs.items():
+        expected_dcr_df = pd.DataFrame({"dcr_1": expected_dcr}).astype("float32")
+
+        dcr_df = dcr(
+            source_df,
+            target_df,
+            device="cpu",
+            k=1,
+            metric=metric,
+            standardize=False,
+        )
+        pd.testing.assert_frame_equal(
+            dcr_df, expected_dcr_df, check_exact=False, atol=1e-5
+        )
+
+
+def test_dcr4():
+    source_df = pd.DataFrame(
+        {
+            "c1": ["a"],
+            "c2": ["1"],
+            "c3": [1.0],
+            "c4": [-1.0],
+        }
+    )
+
+    target_df = pd.DataFrame(
+        {
+            "c1": ["a"],
+            "c2": ["2"],
+            "c3": [1.0],
+            "c4": [4.0],
+        }
+    )
+
+    v1 = torch.tensor([1.0, 0.0, 1.0, 0.0, 1.0, -1.0])
+    v2 = torch.tensor([1.0, 0.0, 0.0, 1.0, 1.0, 4.0])
+
+    expected_dcrs = {
+        "l1": torch.abs(v1 - v2).sum().item(),
+        "euclidean": torch.sqrt(((v1 - v2) ** 2).sum()).item(),
+        "cosine": 1 - (torch.dot(v1, v2) / (torch.norm(v1) * torch.norm(v2))).item(),
+    }
+
+    for metric, expected_dcr in expected_dcrs.items():
+        dcr_df = dcr(
+            source_df,
+            target_df,
+            device="cpu",
+            k=1,
+            metric=metric,
+            standardize=False,
+        )
+        assert dcr_df.iloc[0, 0] == expected_dcr
--- torch_dcr/dcr.py
+++ torch_dcr/dcr.py
@@ -3,6 +3,78 @@ import torch
 from tqdm import tqdm
 
 
+def hybrid_cosine_distance(x_real, x_cat, y_real, y_cat):
+    """Computes the hybrid cosine distance between two sets of records.
+
+    Args:
+        x_real (torch.Tensor): A tensor containing the continuous features of
+        the first set of records. x_cat (torch.Tensor): A tensor containing the
+        categorical features of the first set of records. y_real (torch.Tensor):
+        A tensor containing the continuous features of the second set of
+        records. y_cat (torch.Tensor): A tensor containing the categorical
+        features of the second set of records.
+
+    Returns:
+        torch.Tensor: A tensor containing the hybrid cosine distances between
+        each pair of records from the two sets.
+    """
+    cat_dim = x_cat.shape[-1]
+    real_dot_product = torch.matmul(x_real, y_real.t())
+    cat_dot_product = (x_cat.unsqueeze(1) == y_cat.unsqueeze(0)).sum(-1).float()
+    x_norm = torch.sqrt((x_real**2).sum(dim=-1) + cat_dim)
+    y_norm = torch.sqrt((y_real**2).sum(dim=-1) + cat_dim)
+    return 1 - (real_dot_product + cat_dot_product) / (
+        x_norm.unsqueeze(1) * y_norm.unsqueeze(0)
+    )
+
+
+def hybrid_euclidean_distance(x_real, x_cat, y_real, y_cat):
+    """Computes the hybrid Euclidean distance between two sets of records.
+
+    Args:
+        x_real (torch.Tensor): A tensor containing the continuous features of
+        the first set of records. x_cat (torch.Tensor): A tensor containing the
+        categorical features of the first set of records. y_real (torch.Tensor):
+        A tensor containing the continuous features of the second set of
+        records. y_cat (torch.Tensor): A tensor containing the categorical
+        features of the second set of records.
+
+    Returns:
+        torch.Tensor: A tensor containing the hybrid Euclidean distances between
+        each pair of records from the two sets.
+    """
+    real_l2_distance = ((x_real.unsqueeze(1) - y_real.unsqueeze(0)) ** 2).sum(-1)
+    cat_l2_distance = (x_cat.unsqueeze(1) != y_cat.unsqueeze(0)).sum(-1).float() * 2
+    return torch.sqrt(real_l2_distance + cat_l2_distance)
+
+
+def hybrid_l1_distance(x_real, x_cat, y_real, y_cat):
+    """Computes the hybrid L1 distance between two sets of records.
+
+    Args:
+        x_real (torch.Tensor): A tensor containing the continuous features of
+        the first set of records. x_cat (torch.Tensor): A tensor containing the
+        categorical features of the first set of records. y_real (torch.Tensor):
+        A tensor containing the continuous features of the second set of
+        records. y_cat (torch.Tensor): A tensor containing the categorical
+        features of the second set of records.
+
+    Returns:
+        torch.Tensor: A tensor containing the hybrid L1 distances between each
+        pair of records from the two sets.
+    """
+    real_l1_distance = torch.abs(x_real.unsqueeze(1) - y_real.unsqueeze(0)).sum(-1)
+    cat_l1_distance = (x_cat.unsqueeze(1) != y_cat.unsqueeze(0)).sum(-1).float() * 2
+    return real_l1_distance + cat_l1_distance
+
+
+DISTANCES = {
+    "cosine": hybrid_cosine_distance,
+    "euclidean": hybrid_euclidean_distance,
+    "l1": hybrid_l1_distance,
+}
+
+
 def dcr(
     source_df: pd.DataFrame,
     target_df: pd.DataFrame,
@@ -12,7 +84,8 @@ def dcr(
     batch_size: int = 1000,
     metric: str = "cosine",
     output_indexes: bool = False,
-) -> pd.DataFrame:
+    progress_bar: bool = True,
+) -> pd.DataFrame | tuple[pd.DataFrame, pd.DataFrame]:
     """Computes the distance to the closest record (DCR) between two dataframes.
 
     For each record in the source dataframe, it outputs the distances to the k
@@ -30,10 +103,11 @@ def dcr(
         performed using the mean and standard deviation of the target dataset.
         Defaults to True. batch_size (int, optional): The size of the batches
         for processing. Defaults to 1000. metric (str, optional): The metric to
-        use for computing distances. Options are "cosine" and "euclidean".
+        use for computing distances. Options are "cosine", "euclidean" and "l1".
         Defaults to "cosine". output_indexes (bool, optional): Whether to output
         the indexes of the closest records. If true a tuple of (distances,
-        indexes) is returned. Defaults to False.
+        indexes) is returned. Defaults to False. progress_bar (bool, optional):
+        Whether to display a progress bar. Defaults to True.
 
     Returns:
         pd.DataFrame: A dataframe containing the distances to the k closest
@@ -42,42 +116,47 @@ def dcr(
         containing the distances and indexes is a dataframe containing the
         indexes of the closest records in the target dataframe.
     """
-    # Avoid modifying the original dataframes
-    source_df = source_df.copy()
-    target_df = target_df.copy()
+    if metric not in DISTANCES:
+        raise ValueError(
+            f"Invalid metric '{metric}'. Valid options are: {list(DISTANCES.keys())}"
+        )
+    hybrid_distance = DISTANCES[metric]
+
+    source_delimiter = len(source_df)
+    common_df = pd.concat([source_df.copy(), target_df.copy()], ignore_index=True)
 
     # Detect categorical columns
-    cat_columns = source_df.select_dtypes(
+    cat_columns = common_df.select_dtypes(
         include=["category", "object", str]
     ).columns.tolist()
 
     # Convert object, str and categorical columns to categorical
     for col in cat_columns:
-        source_df[col] = source_df[col].astype("category")
-        target_df[col] = target_df[col].astype("category")
+        common_df[col] = common_df[col].astype("category")
 
     # Convert categorical columns to numerical codes
     for col in cat_columns:
-        source_df[col + "_number"] = source_df[col].cat.codes
-        target_df[col + "_number"] = target_df[col].cat.codes
-        source_df = source_df.drop(columns=[col])
-        target_df = target_df.drop(columns=[col])
-        source_df = source_df.rename(columns={col + "_number": col})
-        target_df = target_df.rename(columns={col + "_number": col})
+        common_df[col + "_number"] = common_df[col].cat.codes
+        common_df = common_df.drop(columns=[col])
+        common_df = common_df.rename(columns={col + "_number": col})
 
     # Convert to tensors
     source_continuous = torch.tensor(
-        source_df.drop(columns=cat_columns).values, dtype=torch.float32
+        common_df.iloc[:source_delimiter].drop(columns=cat_columns).values,
+        dtype=torch.float32,
     ).to(device)
     target_continuous = torch.tensor(
-        target_df.drop(columns=cat_columns).values, dtype=torch.float32
+        common_df.iloc[source_delimiter:].drop(columns=cat_columns).values,
+        dtype=torch.float32,
     ).to(device)
     source_categorical = torch.tensor(
-        source_df[cat_columns].values, dtype=torch.int64
+        common_df.iloc[:source_delimiter][cat_columns].values, dtype=torch.int32
     ).to(device)
     target_categorical = torch.tensor(
-        target_df[cat_columns].values, dtype=torch.int64
+        common_df.iloc[source_delimiter:][cat_columns].values, dtype=torch.int32
     ).to(device)
+
+    del common_df  # Free up memory
 
     if standardize:
         # using the mean and std of the target dataset
@@ -88,36 +167,28 @@ def dcr(
 
     distance_matrix = torch.zeros((len(source_df), k)).to(device)
 
+    indices_matrix = None
     if output_indexes:
         indices_matrix = torch.zeros((len(source_df), k), dtype=torch.int64).to(device)
 
-    for i in tqdm(range(0, len(source_df), batch_size)):
+    for i in tqdm(range(0, len(source_df), batch_size), disable=not progress_bar):
         start = i
         end = min((i + 1) + batch_size, len(source_df))
 
-        batch_continuous = source_continuous[start:end]
-        batch_categorical = source_categorical[start:end]
-
-        distance = torch.zeros((end - start, len(target_df))).to(device)
-
-        if metric == "cosine":
-            distance += 1 - torch.nn.functional.cosine_similarity(
-                batch_continuous.unsqueeze(1), target_continuous.unsqueeze(0), dim=-1
-            )
-        elif metric == "euclidean":
-            distance += torch.cdist(batch_continuous, target_continuous, p=2)
-
-        distance += (
-            batch_categorical.unsqueeze(1) != target_categorical.unsqueeze(0)
-        ).sum(-1)
+        distance = hybrid_distance(
+            source_continuous[start:end],
+            source_categorical[start:end],
+            target_continuous,
+            target_categorical,
+        )
 
         distance_matrix[start:end] = torch.topk(distance, k=k, largest=False)[0]
-        if output_indexes:
+        if indices_matrix is not None:
             indices_matrix[start:end] = torch.topk(distance, k=k, largest=False)[1]
 
     distances = distance_matrix.cpu().numpy()
     dcr_df = pd.DataFrame(distances, columns=[f"dcr_{i + 1}" for i in range(k)])
-    if output_indexes:
+    if indices_matrix is not None:
         index_df = pd.DataFrame(
             indices_matrix.cpu().numpy(), columns=[f"index_{i + 1}" for i in range(k)]
         )
